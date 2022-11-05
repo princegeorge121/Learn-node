16a08c6a84a9a2fc2d12bb1599936a0e34c1beb6
Prince George <prince.george@experionglobal.local>
2022-11-05 14:24:53 +0530

    my first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Hello node app");
+console.log("Hello node apppppp");
+
+// const http=require("http");
+// var app=http.createServer();
+
+// var passenger={
+//     fname:"passenger 1",
+//     email:"passenger1@mail.com",
+//     age:45,
+//     phone:[124,1234]
+// };
+// var data=JSON.stringify(passenger);
+// console.log(data);
+
+// var text="{"fname":"passenger 1","email":"passenger1@mail.com","age":45,"phone":[124,1234]}"
+// var data=JSON.parse(text);
+// console.log(data);
+
+
+
+
+
